fd54a8c70b5f927ddf0b0aa7500f2ce2932d0f5a
Seimen <seimen@cloasdata.de>
2023-05-30 18:13:30 +0200

    general update

--- test/test_server.hpp
+++ test/test_server.hpp
@@ -9,22 +9,22 @@
 
 using provideType = ProviderRS485<HardwareSerial>;
 
-Scheduler testScheduler{};
+Scheduler serverScheduler{};
 
 void GivenStreamWhenNewInstanceThenNoError(){
     ProviderRS485<HardwareSerial> provider{Serial, 0};
     
-    ModbusServer<ProviderRS485<HardwareSerial>> server {&testScheduler, &provider, 1};
+    ModbusServer<ProviderRS485<HardwareSerial>> server {&serverScheduler, &provider, 1};
     assert (server.slaveAddress() == 1);
 }
 
 void GivenServer_WhenStartedandRun_RaiseNoException(){
     ProviderRS485<HardwareSerial> provider{Serial, 0};
-    ModbusServer<ProviderRS485<HardwareSerial>> server {&testScheduler, &provider, 1};
+    ModbusServer<ProviderRS485<HardwareSerial>> server {&serverScheduler, &provider, 1};
     
     server.start();
     for (int i = 0; i < 100; i++){
-        testScheduler.execute();
+        serverScheduler.execute();
     }
     
 
@@ -33,7 +33,7 @@ void GivenServer_WhenStartedandRun_RaiseNoException(){
 
 void GivenServerWhenResponseAddedThenNoError(){
     ProviderRS485<HardwareSerial> provider{Serial, 0};
-    ModbusServer<ProviderRS485<HardwareSerial>> server {&testScheduler, &provider, 1};
+    ModbusServer<ProviderRS485<HardwareSerial>> server {&serverScheduler, &provider, 1};
 
     server.responseTo(03, 0, [](ModbusResponse<provideType> *response){});
     
@@ -43,14 +43,14 @@ void GivenServerWhenResponseAddedThenNoError(){
 void GivenServer_WhenStarted_ThenNoError(){
     MockStream mock{};
     SerialProvider<MockStream> provider{mock};
-    ModbusServer<SerialProvider<MockStream>> server {&testScheduler, &provider, 1};
+    ModbusServer<SerialProvider<MockStream>> server {&serverScheduler, &provider, 1};
     mock.append(ReadRequest04, sizeof(ReadRequest04));
     mock.begin();
 
     server.responseTo(04, 0x0131, [](ModbusResponse<SerialProvider<MockStream>> *response){});
     server.start();
     while (mock.available()){
-        testScheduler.execute();
+        serverScheduler.execute();
     }
     assert (server.errorCount() == 0);
 }
@@ -58,19 +58,19 @@ void GivenServer_WhenStarted_ThenNoError(){
 void GivenServer_WhenRun_ThenCallback(){
     MockStream mock{};
     SerialProvider<MockStream> provider{mock};
-    ModbusServer<SerialProvider<MockStream>> server {&testScheduler, &provider, 1};
+    ModbusServer<SerialProvider<MockStream>> server {&serverScheduler, &provider, 1};
     mock.append(ReadRequest04, sizeof(ReadRequest04));
     mock.begin();
 
     server.responseTo(04, 0x0131, [](ModbusResponse<SerialProvider<MockStream>> *response){
-        uint8_t payload[1] = {0xFF};
-        response->send(payload);
+        uint8_t payload[2] = {0x00, 0xFF};
+        response->send(payload, 2);
     });
 
     server.start();
     
     while (!server.getParser().isComplete() && !server.getParser().isError()){
-        testScheduler.execute();
+        serverScheduler.execute();
     }
     assert(mock.writeBuffer()[3] == 0xFF);
 }
@@ -78,20 +78,20 @@ void GivenServer_WhenRun_ThenCallback(){
 void Given03Handler_WhenRequest04_ThenExceptionResponse(){
     MockStream mock{};
     SerialProvider<MockStream> provider{mock};
-    ModbusServer<SerialProvider<MockStream>> server {&testScheduler, &provider, 1};
+    ModbusServer<SerialProvider<MockStream>> server {&serverScheduler, &provider, 1};
     mock.append(ReadRequest04, sizeof(ReadRequest04));
     mock.begin();
 
     // 03 fc is not requested
     server.responseTo(03, 0x0131, [](ModbusResponse<SerialProvider<MockStream>> *response){
-        uint8_t payload[1] = {0xFF};
-        response->send(payload);
+        uint8_t payload[2] = {0x00, 0xFF};
+        response->send(payload, 2);
         ESP.restart();
     });
 
     server.start();
     while (!server.getParser().isComplete() && !server.getParser().isError()){
-        testScheduler.execute();
+        serverScheduler.execute();
     }
 
     assert(mock.writeBuffer()[1] == 04 + 128); // exception response
@@ -100,19 +100,19 @@ void Given03Handler_WhenRequest04_ThenExceptionResponse(){
 void Given130AddressHandler_WhenRequest130_ThenExceptionResponse(){
     MockStream mock{};
     SerialProvider<MockStream> provider{mock};
-    ModbusServer<SerialProvider<MockStream>> server {&testScheduler, &provider, 1};
+    ModbusServer<SerialProvider<MockStream>> server {&serverScheduler, &provider, 1};
     mock.append(ReadRequest04, sizeof(ReadRequest04));
     mock.begin();
 
     // address 131 not available
     server.responseTo(04, 0x0130, [](ModbusResponse<SerialProvider<MockStream>> *response){
-        uint8_t payload[1] = {0xFF};
-        response->send(payload);
+        uint8_t payload[2] = {0x00, 0xFF};
+        response->send(payload, 2);
     });
 
     server.start();
     while (!server.getParser().isComplete() && !server.getParser().isError()){
-        testScheduler.execute();
+        serverScheduler.execute();
     }
     assert(mock.writeBuffer()[1] == 04 + 128); // exception response
     assert(mock.writeBuffer()[2] == 2);
@@ -121,19 +121,19 @@ void Given130AddressHandler_WhenRequest130_ThenExceptionResponse(){
 void GivenServerVerboseParser_WhenRequestCRCBad_ThenExceptionResponse(){
     MockStream mock{};
     SerialProvider<MockStream> provider{mock};
-    ModbusServer<SerialProvider<MockStream>> server {&testScheduler, &provider, 1};
+    ModbusServer<SerialProvider<MockStream>> server {&serverScheduler, &provider, 1};
     mock.append(BadCRCRequest04, sizeof(BadCRCRequest04));
     mock.begin();
     server.verboseParser(true); // parser errors are ignored by default
 
     server.responseTo(04, 0x0131, [](ModbusResponse<SerialProvider<MockStream>> *response){
-        uint8_t payload[1] = {0xFF};
-        response->send(payload);
+        uint8_t payload[2] = {0x00 ,0xFF};
+        response->send(payload, 2);
     });
 
     server.start();
     while (!server.getParser().isComplete() && !server.getParser().isError()){
-        testScheduler.execute();
+        serverScheduler.execute();
     }
 
     assert(mock.writeBuffer()[1] == 04 + 128); // exception response
@@ -143,31 +143,31 @@ void GivenServerVerboseParser_WhenRequestCRCBad_ThenExceptionResponse(){
 void GivenUserErrorHandler_WhenRequest04_ThenExceptionResponse(){
     MockStream mock{};
     SerialProvider<MockStream> provider{mock};
-    ModbusServer<SerialProvider<MockStream>> server {&testScheduler, &provider, 1};
+    ModbusServer<SerialProvider<MockStream>> server {&serverScheduler, &provider, 1};
     mock.append(ReadRequest04, sizeof(ReadRequest04));
     mock.begin();
 
-    server.setOnExceptionHandler([](ModbusExceptionResponse<SerialProvider<MockStream>> *response){
+    server.setOnError([](ModbusExceptionResponse<SerialProvider<MockStream>> *response){
         assert(response->functionCode() == 04);
         response->sendException();
     });
     
     //request is 04
     server.responseTo(03, 0x0131, [](ModbusResponse<SerialProvider<MockStream>> *response){
-        uint8_t payload[1] = {0xFF};
-        response->send(payload);
+        uint8_t payload[2] = {0x00, 0xFF};
+        response->send(payload, 2);
     });
 
     server.start();
     while (!server.getParser().isComplete() && !server.getParser().isError()){
-        testScheduler.execute();
+        serverScheduler.execute();
     }
     assert(mock.writeBuffer()[1] == 04 + 128); 
     assert(mock.writeBuffer()[2] == 1); 
 }
 
 void runServerTest(){
-    printf("\n\n -- TEST STARTING -- \n\n");
+    printf("\n\n -- Testing Modernbus Server -- \n\n");
     uint16_t heapConsumed = ESP.getFreeHeap();
     unsigned long runningTime = millis();
 
@@ -195,8 +195,5 @@ void runServerTest(){
     printf("HEAP Consumed: %u\n", heapConsumed - ESP.getFreeHeap());
     printf("\nTime: %lu\n", runningTime);
     
-    printf("TEST DONE. BYE");
-    
-    delay(1000);
-    ESP.restart();
+    printf("-- Modernbus Server Tested --");
 }
